1708825344bc8a24b042ffc50246bf148c3a55f6
cersar <1319839558@qq.com>
2021-05-17 22:24:25 +0800

    modify https://github.com/google/automl for easy use

---
+++ efficientnetv2/efficientnetv2.py
@@ -0,0 +1,31 @@
+from .effnetv2_model import EffNetV2Model
+import tensorflow as tf
+import os
+
+def EfficientNetV2(
+        model_name='efficientnetv2-s',
+        weights=None,
+        input_shape=None,
+        include_top=True,
+        dropout_rate=None,
+        num_class=1000):
+    model = EffNetV2Model(model_name=model_name,include_top=include_top)
+    if not input_shape:
+        size = model._mconfig.eval.isize
+        input_shape = (size, size, 3)
+    x = tf.keras.Input(input_shape)
+    output = model.call(x, training=None)
+
+    if not include_top and num_class:
+        if dropout_rate:
+            output = tf.keras.layers.Dropout(dropout_rate)(output)
+        output = tf.keras.layers.Dense(num_class)(output)
+
+    model = tf.keras.Model(inputs=x,outputs=output)
+
+    if os.path.exists(weights):
+        model.load_weights(weights,by_name=True,skip_mismatch=True)
+    else:
+        raise ValueError('invalid weights path: {}!'.format(weights))
+
+    return model
